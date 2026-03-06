46ba8a8b6ca6775acd0ccf4f14efc15afc7fe0f9
gruszek-i <185793537+gruszek-i@users.noreply.github.com>
2026-03-06 09:33:43 +0100

    add 99 plugin for nvim

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+    },
+    panel = { enabled = false },
+  },
+}
